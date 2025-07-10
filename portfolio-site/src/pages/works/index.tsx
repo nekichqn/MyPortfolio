4f18d651cbe0ff72fde118481fc0a9c10e9e4922
Neki <schqn.oo@gmail.com>
2025-07-10 12:43:33 +0900

    worksページの追加

--- portfolio-site/src/pages/works/index.tsx
+++ portfolio-site/src/pages/works/index.tsx
@@ -9,37 +9,73 @@ export default function Works() {
         {/* 作品1 */}
         <div className="bg-gray-100 p-4 rounded-lg shadow hover:shadow-md transition">
           <Image
-            src="/works/work1.jpg"
+            src="/img/works/web01.png"
             alt="作品1"
             width={400}
             height={300}
             className="rounded"
           />
-          <p className="mt-2 text-sm text-gray-600">Illustrator / チラシデザイン</p>
+          <p className="mt-2 text-sm text-gray-600">01.Webサイト</p>
         </div>
 
         {/* 作品2 */}
         <div className="bg-gray-100 p-4 rounded-lg shadow hover:shadow-md transition">
           <Image
-            src="/works/work2.jpg"
+            src="/img/works/web02.png"
             alt="作品2"
             width={400}
             height={300}
             className="rounded"
           />
-          <p className="mt-2 text-sm text-gray-600">Photoshop / バナー制作</p>
+          <p className="mt-2 text-sm text-gray-600">02.Webサイト</p>
         </div>
 
         {/* 作品3 */}
         <div className="bg-gray-100 p-4 rounded-lg shadow hover:shadow-md transition">
           <Image
-            src="/works/work3.jpg"
+            src="/img/works/web02.png"
             alt="作品3"
             width={400}
             height={300}
             className="rounded"
           />
-          <p className="mt-2 text-sm text-gray-600">Webデザイン / LP</p>
+          <p className="mt-2 text-sm text-gray-600">03.Webサイトデザイン案</p>
+        </div>
+
+        {/* 作品4 */}
+        <div className="bg-gray-100 p-4 rounded-lg shadow hover:shadow-md transition">
+          <Image
+            src="/img/works/ill01.png"
+            alt="作品3"
+            width={400}
+            height={300}
+            className="rounded"
+          />
+          <p className="mt-2 text-sm text-gray-600">04.illustrator</p>
+        </div>
+
+        {/* 作品5 */}
+        <div className="bg-gray-100 p-4 rounded-lg shadow hover:shadow-md transition">
+          <Image
+            src="/img/works/ill02.png"
+            alt="作品3"
+            width={400}
+            height={300}
+            className="rounded"
+          />
+          <p className="mt-2 text-sm text-gray-600">05.illustrator</p>
+        </div>
+
+        {/* 作品6 */}
+        <div className="bg-gray-100 p-4 rounded-lg shadow hover:shadow-md transition">
+          <Image
+            src="/img/works/bl01.png"
+            alt="作品3"
+            width={400}
+            height={300}
+            className="rounded"
+          />
+          <p className="mt-2 text-sm text-gray-600">06.3DModeling</p>
         </div>
       </div>
     </main>
